9c32d6a050984daab5e3d7c3b418658017d8a91a
Chris Burnett <dameramu@gmail.com>
2013-03-13 16:19:48 +0000

    Fixed web.xml

--- IndivoWF/src/uk/ac/dotrural/trump/indivowf/test/TestClient.java
+++ IndivoWF/src/uk/ac/dotrural/trump/indivowf/test/TestClient.java
@@ -7,33 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.smartplatforms.client.tests.Reminder;
+
 /**
  * Servlet implementation class TestClient
  */
-@WebServlet(description = "Just a test client for Indivo using the reminders app.", urlPatterns = { "/TestClient" })
-public class TestClient extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestClient() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
+public class TestClient extends Reminder {
+	
 }
